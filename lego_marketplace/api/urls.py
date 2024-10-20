3127d4b3089df0b5273edb356d715ea219a788d8
tomasrodriguess <tomasdcrodrigues@gmail.com>
2024-10-20 17:55:19 +0100

    Add basic code for users app

--- lego_marketplace/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('notes/', views.NoteListCreate.as_view(), name='note-list'),
-    path('notes/delete/<int:pk>/', views.NoteDeleteView.as_view(), name='note-delete'),
-
-]
---
+++ lego_marketplace/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
+
+urlpatterns = [
+    path('register', CreateUserView.as_view(),name='register'),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api-auth/', include('rest_framework.urls')),
+]
