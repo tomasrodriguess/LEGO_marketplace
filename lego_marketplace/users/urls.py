187a1191239d173f0910feabb59610a9aac5650c
tomasrodriguess <tomasdcrodrigues@gmail.com>
2024-10-20 22:31:21 +0100

    Add route to update user email

--- lego_marketplace/users/urls.py
+++ lego_marketplace/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import CreateUserView
+from .views import CreateUserView, UserProfileView, UserProfileView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
 
 urlpatterns = [
     path('register', CreateUserView.as_view(),name='register'),
-    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('login', TokenObtainPairView.as_view(), name='get_token'),
+    path('profile', UserProfileView.as_view(), name='user_profile'),
+    path('update', UserProfileView.as_view(), name='update_user'),
+    path('token/refresh', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include('rest_framework.urls')),
 ]
--- lego_marketplace/users/views.py
+++ lego_marketplace/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
@@ -9,3 +11,24 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        user = User.objects.filter(id=self.request.user.id).first()
+        return user
+    
+    def update(self, request, *args, **kwargs):
+        user = self.request.user
+        new_email = request.data.get('email')
+
+        if new_email:
+            user.email = new_email
+            user.save()
+            return Response({'detail': 'Email updated successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Email field is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+#ROUTE BELLOR TO RESET PASSWORD
