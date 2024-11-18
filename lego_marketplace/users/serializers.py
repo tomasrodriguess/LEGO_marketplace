a3f49ac626656c138b119529d19d755f1ccbbb8f
tomasrodriguess <tomasdcrodrigues@gmail.com>
2024-11-18 23:14:23 +0000

    Fix user serializer not storing password

--- lego_marketplace/users/serializers.py
+++ lego_marketplace/users/serializers.py
@@ -2,11 +2,18 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email','is_moderator']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password', 'is_staff', 'is_moderator']
+        extra_kwargs = {
+            'password': {'write_only': True}  # Make sure the password isn't returned in responses
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # Hash the password
+        user.save()
         return user
